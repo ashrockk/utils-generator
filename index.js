7eb9140e990f21badcb82d2cdc0b6f7b4e26e0b8
SaurabhKarki <saurabh.karki98176@gmail.com>
2023-06-27 14:07:29 +0545

    npn i ejs

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const ejs = require('ejs');
 
 const app = express();
 
 const indexRouter = require('./routes');
 
-app.use((req, res, next) => {
-    
-})
+//Setting up third-party middlewares
+app.use(morgan('short'));
+app.use(cors());
+
+//Setting up EJS Templating
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use('/', indexRouter);
 
 app.listen(8000, () => {
   console.log("Server running on port 8000");
