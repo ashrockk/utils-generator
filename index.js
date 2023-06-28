9d3de09efe4f1be27bf437325ea2ca64112901a7
SaurabhKarki <saurabh.karki98176@gmail.com>
2023-06-28 12:48:46 +0545

    html/css via node

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
-const express = require('express');
-const morgan = require('morgan');
-const cors = require('cors');
-const ejs = require('ejs');
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const ejs = require("ejs");
 
 const app = express();
 
-const indexRouter = require('./routes');
+const indexRouter = require("./routes");
 
-//Setting up third-party middlewares
-app.use(morgan('short'));
+// Setting up the third party middlewares
+app.use(morgan("short"));
 app.use(cors());
 
-//Setting up EJS Templating
-app.set('view engine', 'ejs');
-app.set('views', './views');
+// Setting up the EJS Templating
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
-app.use('/', indexRouter);
+//serving the static files
+
+app.use(express.static("public"));
+
+app.use("/", indexRouter);
 
 app.listen(8000, () => {
   console.log("Server running on port 8000");
