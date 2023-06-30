f6d3eb8d6f0fc7cb87a9839d0d20fc77f1cfa68d
SaurabhKarki <saurabh.karki98176@gmail.com>
2023-06-30 13:40:11 +0545

    debugged by ecabott

--- index.js
+++ index.js
@@ -15,22 +15,21 @@ app.use(cors());
 app.set("view engine", "ejs");
 app.set("views", "./views");
 
-//serving the static files
-
-app.use(express.static("public"));
-app.get("/broken", (req, res, next) => {
-  throw new Error("Broken");
-});
-  
+// Serving the static files
+app.use(express.static('public'));
 
 app.use("/", indexRouter);
 
-//Application level error handler
-app.user(err, req, res, next) => {
-  console.log(err);
-  res.status(500).send("Something is wrong but IDK.");
-};
+// To test application level error handling
+// app.get('/broken', (req, res, next) => {
+//   throw new Error('Broken');
+// });
 
+// Application level error handling
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).send('Something went wrong');
+});
 
 app.listen(8000, () => {
   console.log("Server running on port 8000");
