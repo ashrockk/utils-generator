6f3ef05d51cc90b9be328742ce52cce4418f235b
SaurabhKarki <saurabh.karki98176@gmail.com>
2023-06-30 13:42:53 +0545

    routes commit

--- routes/routes.api.js
+++ routes/routes.api.js
@@ -4,5 +4,7 @@ const router = express.Router();
 router.get("/", (req, res) => {
   res.json({ msg: "Hello from API" });
 });
-
+router.post("/form",(req,res,next) => {
+res.json({msg:"Hello from form api"});
+});
 module.exports = router;
