6a4950881d8b4503322ca2947bf4ffc0c7f2250b
SaurabhKarki <saurabh.karki98176@gmail.com>
2023-06-30 15:06:01 +0545

    made form and set backend

--- routes/routes.api.js
+++ routes/routes.api.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json({ msg: "Hello from API" });
+router.get("/", (req, res, next) => {
+  try {
+    // Database query goes here
+    // Utils query
+    res.json({ msg: "Hello from API" });
+  } catch (err) {
+    next(err);
+  }
 });
-router.post("/form",(req,res,next) => {
-res.json({msg:"Hello from form api"});
+
+router.post('/form', (req, res, next) => {
+  console.log({ data: req.body});
+  res.json({msg: "Thank you from submitting!!!!!!"});
 });
+
 module.exports = router;
